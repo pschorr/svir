6cbfcfb86d7154effd377e4e1dfaa3cf1dbe0426
Amy Leung <amy.wl.leung@gmail.com>
2017-11-17 14:45:37 -0500

    Updated with input file UI backend

--- R/server.R
+++ R/server.R
@@ -1,6 +1,31 @@
 library(shiny)
+library(maptools)
 
 # Define server logic
 shinyServer(function(input, output) {
 
+  uploadShpfile <- reactive({
+    if (!is.null(input$shp)){
+      shpDF <- input$shp
+      prevWD <- getwd()
+      updir <- dirname(shpDF$datapath[1])
+      setwd(updir)
+      for (i in 1:nrow(shpDF)){
+        file.rename(shpDF$datapath[i], shpDF$name[i])
+      }
+      shpName <- shpDF$name[grep(x = shpDF$name, pattern = "*.shp")]
+      shpPath <- paste(updir, shpName, sep = "/")
+      setwd(prevWD)
+      shpFile <- readShapePoly(shpPath)
+      return(shpFile)
+    } else {
+      return()
+    }
+  })
+
+  output$map <- renderPlot({
+    if (!is.null(uploadShpfile())){
+      plot(uploadShpfile())
+    }
+  })
 })
