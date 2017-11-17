deac71bb7e6b040b8e4ccbdfe05f8e7a975922b5
Amy Leung <amy.wl.leung@gmail.com>
2017-11-17 14:45:22 -0500

    Updated with input file UI

--- R/ui.R
+++ R/ui.R
@@ -1,7 +1,18 @@
 library(shiny)
 
 # Define UI for file upload
-shinyUI(fluidPage(
+shinyUI(pageWithSidebar(
 
+  # add application title
+  headerPanel(""),
 
+  # sidebar panel with file upload
+  sidebarPanel(
+    fileInput(inputId="shp", label="Upload Shapefile", multiple=TRUE)),
+
+   # plot shapefile
+   mainPanel(
+    plotOutput("map")
+  )
 ))
+
