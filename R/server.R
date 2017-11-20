dbcb9017ecc14236cb70daa985ef838525999c7b
Amy Leung <amy.wl.leung@gmail.com>
2017-11-20 16:37:21 -0500

    Fixed broken pgGetGeom() args

--- R/server.R
+++ R/server.R
@@ -4,35 +4,52 @@ library(rpostgis)
 library(RPostgreSQL)
 library(sp)
 
-# create a PostgreSQL instance and create one connection
-drv <- dbDriver("PostgreSQL")
-
-# open the connection with credentials
-con <- dbConnect(drv, user="postgres", password="gisde2018",
-                 host="localhost", port=5432,dbname="svir")
-
 # Define server logic
 shinyServer(function(input, output) {
 
+  # create a PostgreSQL instance and create one connection
+  drv <- dbDriver("PostgreSQL")
+
+  # open the connection with credentials
+  con <- dbConnect(
+    drv,
+    user = "postgres",
+    password = "gisde2018",
+    host = "localhost",
+    port = 5432,
+    dbname = "svir"
+  )
+
   # create upload file function
   uploadShpfile <- reactive({
-    if (!is.null(input$shp)){
+    if (!is.null(input$shp)) {
       shpDF <- input$shp
       prevWD <- getwd()
       updir <- dirname(shpDF$datapath[1])
       setwd(updir)
-      for (i in 1:nrow(shpDF)){
+      for (i in 1:nrow(shpDF)) {
         file.rename(shpDF$datapath[i], shpDF$name[i])
       }
       shpName <- shpDF$name[grep(x = shpDF$name, pattern = "*.shp")]
       shpPath <- paste(updir, shpName, sep = "/")
       setwd(prevWD)
       shpFile <- readOGR(shpPath)
-      writeOGR(shpFile, dsn = c("PG:user = 'postgres' dbname = 'svir' host = 'localhost'"),  # write shp to PG table & create sp index
-               layer = "userext", overwrite_layer = TRUE, driver = "PostgreSQL")
-      res <- pgGetGeom(con, name = c("public", "svi2014_us"), geom = "geom", gid = "gid", query = "SELECT *
-                     FROM svi2014_us AS svi, userext AS poly
-                       WHERE ST_Intersects(svi.geom, ST_Transform(poly.wkb_geometry, 4269))")  # find intersections of user input and states and return geom
+      shpFile <- spTransform(shpFile, CRS("+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0"))
+      writeOGR(
+        shpFile,
+        dsn = c("PG:user = 'postgres' dbname = 'svir' host = 'localhost'"),
+        # write shp to PG table & create sp index
+        layer = "userext",
+        overwrite_layer = TRUE,
+        driver = "PostgreSQL"
+      )
+
+      res <- pgGetGeom(  # find intersections of user input and states and return geom
+          con,
+          query = sprintf("SELECT *
+            FROM public.svi2014_us, public.userext
+            WHERE ST_Intersects(public.svi2014_us.geom, public.userext.wkb_geometry);")
+        )
       return(res)
 
     } else {
@@ -42,12 +59,12 @@ shinyServer(function(input, output) {
 
   # plot output$map
   output$map <- renderPlot({
-    if (!is.null(uploadShpfile())){
-      plot(uploadShpfile())
+    if (!is.null(uploadShpfile())) {
+      plot(uploadShpfile(), byId = FALSE)
     }
   })
-})
 
-# disconnect db connection
-dbDisconnect(con)
-dbUnloadDriver(drv)
+  # # disconnect db connection
+  # dbDisconnect(con)
+  # dbUnloadDriver(drv)
+})
