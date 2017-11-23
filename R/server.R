bc47770a2185cb6e78e5157d135b3976c4c1ba65
Amy Leung <amy.wl.leung@gmail.com>
2017-11-22 19:28:12 -0500

    Added leaflet server logic and add user input to map

--- R/server.R
+++ R/server.R
@@ -4,10 +4,10 @@ library(rpostgis)
 library(RPostgreSQL)
 library(sp)
 library(leaflet)
+library(rgeos)
 
 # Define server logic
 shinyServer(function(input, output) {
-
   # create a PostgreSQL instance and create one connection
   drv <- dbDriver("PostgreSQL")
 
@@ -15,10 +15,10 @@ shinyServer(function(input, output) {
   con <- dbConnect(
     drv,
     user = "postgres",
-    password = "pschorrzan1960",
+    password = "gisde2018",
     host = "localhost",
     port = 5432,
-    dbname = "SVI"
+    dbname = "svir"
   )
 
   # create upload file function
@@ -35,22 +35,32 @@ shinyServer(function(input, output) {
       shpPath <- paste(updir, shpName, sep = "/")
       setwd(prevWD)
       shpFile <- readOGR(shpPath)
-      shpFile <- spTransform(shpFile, CRS("+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0"))
+      shpFile <-
+        spTransform(
+          shpFile,
+          CRS(
+            "+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0"
+          )
+        )
       writeOGR(
         shpFile,
-        dsn = c("PG:user = 'postgres' dbname = 'SVI' host = 'localhost'"),
+        dsn = c("PG:user = 'postgres' dbname = 'svir' host = 'localhost'"),
         # write shp to PG table & create sp index
         layer = "userext",
         overwrite_layer = TRUE,
         driver = "PostgreSQL"
       )
 
-      res <- pgGetGeom(  # find intersections of user input and states and return geom
+      res <-
+        pgGetGeom(
+          # find intersections of user input and states and return geom
           con,
-          query = sprintf("SELECT *
+          query = sprintf(
+            "SELECT *
             FROM public.svi2014_us, public.userext
-            WHERE ST_Intersects(public.svi2014_us.geom, public.userext.wkb_geometry);")
-        )
+            WHERE ST_Intersects(public.svi2014_us.geom, public.userext.wkb_geometry);"
+          )
+          )
       return(res)
 
     } else {
@@ -58,10 +68,20 @@ shinyServer(function(input, output) {
     }
   })
 
-  # plot output$map
-  output$map <- renderPlot({
+  # add leaflet map
+  output$map <- renderLeaflet({
+    leaflet(width = "100%", height = "100%") %>%
+      addProviderTiles(provider = "CartoDB.Positron") %>%
+      setView(-98.6106479, 39.8123024, zoom = 4)
+  })
+
+  # add polygons
+  observeEvent(input$shp, {
     if (!is.null(uploadShpfile())) {
-      plot(uploadShpfile(), byId = FALSE)
+      cent <- gCentroid(spgeom = uploadShpfile(), byid = FALSE)
+      leafletProxy("map") %>%
+        addPolygons(data = uploadShpfile()) %>%
+        setView(lat = slot(cent, "coords")[[2]], lng = slot(cent, "coords")[[1]], zoom = 5)
     }
   })
 
